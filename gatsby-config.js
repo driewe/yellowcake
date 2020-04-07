507f8993fd93fa2af723e88621e5b4470168c9bc
David Riewe <david.riewe@gmail.com>
2020-04-06 19:07:23 -0500

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yellowcake',
-    siteUrl: 'https://yellowcake.netlify.com'
+    title: 'Experiential Gardener',
+    siteUrl: 'https://experientialgardener.netlify.com'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'yellowcake',
-        short_name: 'yellowcake',
+        name: 'Experiential Gardener',
+        short_name: 'Experiential Gardener',
         start_url: '/',
         background_color: '#00C2BD',
         theme_color: '#00C2BD',
