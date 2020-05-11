806430c66ed4508e04a9efd71205cd4e55914bb7
David Riewe <david.riewe@gmail.com>
2020-05-11 11:45:44 -0500

    added disqus

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    siteTitle: 'Riewe Web Development', // Site title.
-    siteTitleAlt: 'Business', // Alternative site title for SEO.
+    siteTitle: 'Experiential Gardener', // Site title.
+    siteTitleAlt: 'Gardening', // Alternative site title for SEO.
     siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-    siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
+    siteUrl: 'https://www.experientialgardener.com', // Domain of your website without pathPrefix.
     pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-    siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
+    siteDescription: 'My name is Rebecca and I am the Experiential Gardener. I have been gardening for over forty years and live in North Central Texas, in Hardiness Zone 8a.', // Website description used for RSS feeds/meta description tag.
     siteRss: '/rss.xml',
     siteFBAppID: '', // FB Application ID for using app insights
     googleTagManagerID: '', // GTM tracking ID.
     disqusShortname: 'experiential-gardener', // Disqus shortname.
-    userName: 'David Riewe',
-    userTwitter: 'riewedavid',
+    userName: 'Rebecca Barham',
+    userTwitter: 'Texasgardener8a',
     userLocation: 'Denton, Texas USA',
     userDescription: '',
-    copyright: 'Copyright © Gatsby Starter Business 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+    copyright: 'Copyright © Experiential Gardener 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
     themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
     backgroundColor: '#ffffff', // Used for setting manifest background color.
   }
